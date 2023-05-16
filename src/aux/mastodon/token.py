e5239c2baf1102526846b2fdc0e8c82e56321698
jorgeBIGS <jorgeBIGS@users.noreply.github.com>
2023-05-16 16:20:11 +0200

    Update token.py

--- src/aux/mastodon/token.py
+++ src/aux/mastodon/token.py
@@ -1,3 +1,2 @@
 #TODO 0: Fix the app secret token for authentification.
-secret_token = {'mastodon.social':'199-mQZZ_0b7lBHCm58GrEqG01YfxK0WOd3MsaTRHIg',
-                'mastodon.world': 'ISIDLmdtOZe0gfhqZh4mVrm1Np6wQsKCruIjkcj4YNU'}
+secret_token = {'mastodon.social':''}
