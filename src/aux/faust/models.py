989f2737326700479da0acaae30109a58d96add9
jorgeBIGS <jorgeBIGS@users.noreply.github.com>
2023-05-18 11:59:34 +0200

    Update models.py para eliminar elemento redundante

--- src/aux/faust/models.py
+++ src/aux/faust/models.py
@@ -1,9 +1,8 @@
-from aux.misc.utils import read_avro_schema
 from faust import Record
 from fastavro import parse_schema
 
 class Toot(Record):
-    _schema = read_avro_schema()
+    _schema = None
 
 class Message(Record):
     timestamp: float
