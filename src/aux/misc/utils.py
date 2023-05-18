4765cd0a8c8e02ca14085da421e4e03879191498
jorgeBIGS <jorgeBIGS@users.noreply.github.com>
2023-05-18 11:58:29 +0200

    Delete src/aux/misc directory
    Modificación para eliminar script redundante

--- src/aux/misc/utils.py
+++
@@ -1,9 +0,0 @@
-import csv
-import json
-from fastavro import parse_schema
-
-def read_avro_schema():
-    schema = {}
-    with open("../config/avro/mastodon-topic-value.avsc") as file:
-        schema = parse_schema(json.load(file))
-    return schema
